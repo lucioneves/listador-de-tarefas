c0ebe71cf0537eaf5c1bbf3af682bd4b68c90de2
Lucio Neves <luciocecilia20@gmail.com>
2025-08-26 20:53:31 -0300

    chore: add git hooks with prettier and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,24 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig, globalIgnores } from "eslint/config";
+const { rules } = require('eslint-config-prettier')
+const { env } = require('process')
 
-export default defineConfig([
-  globalIgnores(["dist"]),
-  {
-    files: ["**/*.{js,jsx}"],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-      "prettier",
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hook/recommended',
+  ],
+  ignorePatters: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 11, sourceType: 'module' },
+  settigs: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
-        sourceType: "module",
-      },
-    },
-    rules: {
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
-    },
   },
-]);
+}

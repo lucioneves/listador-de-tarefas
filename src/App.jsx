23114daa3acd7a19f305a95aba7d9882a4735e1b
Lucio Neves <luciocecilia20@gmail.com>
2025-09-04 15:17:11 -0300

    feat: update spacing between sidebar and tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Sidebar from './components/Sidebar'
-import Tasks from './components/Tasks'
+import Tasks from './components/tasks'
 
 function App() {
   return (
-    <div className="flex gap-9">
+    <div className="flex">
       <Sidebar />
       <Tasks />
     </div>

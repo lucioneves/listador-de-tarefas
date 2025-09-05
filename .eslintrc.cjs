31a0dd84cb3f1cc4adc40a373bd0ad99d22788b6
Lucio Neves <luciocecilia20@gmail.com>
2025-09-05 14:24:48 -0300

    style: add sort imports plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,9 +13,11 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 11, sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
     'react/jsx-no-target-blank': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
-import SidebarButton from './SidebarButton'
 import { HomeIcon, TasksIcon } from '../assets/icons'
+import SidebarButton from './SidebarButton'
 const Sidebar = () => {
   return (
     <div className="h-screen w-72 bg-white">

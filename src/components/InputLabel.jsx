68db95187ed23be1be5c17fa50e43484b90445fb
Lucio Neves <luciocecilia20@gmail.com>
2025-09-07 18:16:21 -0300

    feat: add InputLabel component

---
+++ src/components/InputLabel.jsx
@@ -0,0 +1,8 @@
+const InputLabel = (props) => {
+  return (
+    <label className="text-sm font-semibold text-[#35383E]" {...props}>
+      {props.children}
+    </label>
+  )
+}
+export default InputLabel

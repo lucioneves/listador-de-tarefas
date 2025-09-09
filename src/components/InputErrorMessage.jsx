4f50ba6a174b86d5baf8b05dc0afadc9ad85e8a6
Lucio Neves <luciocecilia20@gmail.com>
2025-09-09 11:18:51 -0300

    chore: add prop-types InputErrorMessage component

--- src/components/InputErrorMessage.jsx
+++ src/components/InputErrorMessage.jsx
@@ -1,5 +1,9 @@
+import PropTypes from "prop-types"
 const InputErrorMessage = ({ children }) => {
   return <p className="text-left text-xs text-red-500">{children}</p>
+}
+InputErrorMessage.propTypes = {
+  children: PropTypes.string.isRequired,
 }
 
 export default InputErrorMessage

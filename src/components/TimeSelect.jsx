5fffa24461759d95d66ff0f8f32b9581df4ec0f3
Lucio Neves <luciocecilia20@gmail.com>
2025-09-09 11:36:16 -0300

    chore: add prop-types to TimeSelect component

--- src/components/TimeSelect.jsx
+++ src/components/TimeSelect.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types"
 import { forwardRef } from "react"
 
 import InputErrorMessage from "./InputErrorMessage"
@@ -25,4 +26,8 @@ const TimeSelect = forwardRef((props, ref) => {
 })
 
 TimeSelect.displayName = "TimeSelect"
+TimeSelect.propTypes = {
+  errorMessage: PropTypes.string,
+}
+
 export default TimeSelect

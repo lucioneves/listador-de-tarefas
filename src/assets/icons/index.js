8ed5299a3e044e5d9edd73aad8e6d1294da391be
Lucio Neves <luciocecilia20@gmail.com>
2025-09-15 14:22:28 -0300

    feat: add initial TaskDetails page without logic

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,17 +1,21 @@
-import AddIcon from './add.svg?react'
-import CheckIcon from './check.svg?react'
-import CloudSunIcon from './cloud-sun.svg?react'
-import DetailsIcon from './details.svg?react'
-import HomeIcon from './home.svg?react'
-import LoaderIcon from './loader.svg?react'
-import MoonIcon from './moon.svg?react'
-import SunIcon from './sun.svg?react'
-import TasksIcon from './tasks.svg?react'
-import TrashIcon from './trash.svg?react'
+import AddIcon from "./add.svg?react"
+import ArrowLeftIcon from "./arrow-left.svg?react"
+import CheckIcon from "./check.svg?react"
+import ChevronRightIcon from "./chevron-right.svg?react"
+import CloudSunIcon from "./cloud-sun.svg?react"
+import DetailsIcon from "./details.svg?react"
+import HomeIcon from "./home.svg?react"
+import LoaderIcon from "./loader.svg?react"
+import MoonIcon from "./moon.svg?react"
+import SunIcon from "./sun.svg?react"
+import TasksIcon from "./tasks.svg?react"
+import TrashIcon from "./trash.svg?react"
 
 export {
   AddIcon,
+  ArrowLeftIcon,
   CheckIcon,
+  ChevronRightIcon,
   CloudSunIcon,
   DetailsIcon,
   HomeIcon,
